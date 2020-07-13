7df202d0486bf2a0fe8957e74888942777627c87
Gitten <315619439@qq.com>
2020-07-13 21:52:09 +0800

    start 13th class

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from .models import Blog,BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     #context['blogs_count'] = Blog.objects.all().count()
     return render_to_response('blog/blog_list.html',context)
 
@@ -15,6 +16,7 @@ def blog_detail(request,blog_pk):
 def blogs_with_type(request,blog_type_pk):  #blog_type_pk是来自blog/urls.py中第二个path传递的数字
     context = {}
     blog_type = get_object_or_404(BlogType,pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html',context)
