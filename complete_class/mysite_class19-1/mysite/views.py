162f1dc37f642b533b62eccf1280b8d43cf5c4d0
Gitten <315619439@qq.com>
2020-07-20 00:07:53 +0800

    添加虚拟环境

--- complete_class/mysite_class19-1/mysite/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render_to_response,get_object_or_404  #不使用render，改使用render_to_response
-
-def home(request):
-    context = {}
-    return render_to_response('home.html',context)
