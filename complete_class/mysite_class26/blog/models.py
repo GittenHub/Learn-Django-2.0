e0a0abdfaa979b0f6afcce82720be0e5559b3f14
Gitten <315619439@qq.com>
2020-08-10 15:44:44 +0800

    完成第26课

---
+++ complete_class/mysite_class26/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
+from ckeditor_uploader.fields import RichTextUploadingField
+from read_statistics.models import ReadNumExpandMethod, ReadDetail
+
+
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.type_name
+
+class Blog(models.Model, ReadNumExpandMethod):
+    title = models.CharField(max_length=50)
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    read_details = GenericRelation(ReadDetail) # 反向通用关系，这运河之后可直接调用对方了
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Blog: %s" % self.title
+
+    class Meta:
+        ordering = ['-created_time']  # 在最前面加负表示倒序，这里可以设置多个排序方式
