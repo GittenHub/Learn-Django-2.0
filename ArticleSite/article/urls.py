b51ca00d369d6ececfe787f154d5c83ded1160b8
Gitten <315619439@qq.com>
2020-09-05 16:43:37 +0800

    add article with type page

--- ArticleSite/article/urls.py
+++ ArticleSite/article/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_types_list, name='article_types_list'),
+    path('', views.article_list, name='article_list'),
     path('<int:article_pk>', views.article_detail, name='article_detail'),
-    path('type/<int:article_pk>', views.article_with_type, name='article_with_type'),
+    path('type/<int:article_type_pk>', views.article_with_type, name='article_with_type'),
     path('data/<int:article_pk>', views.article_with_date, name='article_with_date'),
 ]
--- ArticleSite/article/views.py
+++ ArticleSite/article/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import ArticleType, Article
 
 # Create your views here.
-def article_types_list(request):
+def article_list(request):
     article_types = ArticleType.objects.all()
+    articles = Article.objects.all()
     context = {}
     context['article_types'] = article_types
-    return render(request, 'article_types_list.html', context)
+    context['articles'] = articles
+    return render(request, 'article_list.html', context)
 
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
@@ -14,8 +16,15 @@ def article_detail(request, article_pk):
     context['article'] = article
     return render(request, 'article_detail.html', context)
 
-def article_with_type(request, article_pk):
-    pass
+def article_with_type(request, article_type_pk):
+    article_types = ArticleType.objects.all()
+    article_type = get_object_or_404(ArticleType, pk=article_type_pk)
+    articles = Article.objects.filter(article_type=article_type)
+    context = {}
+    context['article_type'] = article_type
+    context['article_types'] = article_types
+    context['articles'] = articles
+    return render(request, 'article_with_type.html', context)
 
 def article_with_date(request, article_pk):
     pass
